387e991a65dec4e35720777d5ef6223c8ae8ba15
Hex09AF <lsqhuy@gmail.com>
2023-05-22 22:09:16 +0700

    :bug: fix rocket.lat move

--- stores/map/map.ts
+++ stores/map/map.ts
@@ -4,6 +4,7 @@ interface Rocket {
   id: string;
   lat: number;
   lon: number;
+  aLat: number;
 }
 
 function getLocation({
@@ -32,10 +33,11 @@ export const useMap = defineStore("map", () => {
   const rockets: Rocket[] = [];
   let map: mapboxgl.Map;
 
-  function addRocket(id: string) {
-    let lon = Math.random() * 360 - 180;
-    let lat = Math.random() * 180 - 90;
-
+  function addRocket(
+    id: string,
+    lon: number = Math.random() * 360 - 180,
+    lat: number = Math.random() * 180 - 90
+  ) {
     map.addSource(id, {
       type: "geojson",
       data: getLocation({
@@ -53,7 +55,7 @@ export const useMap = defineStore("map", () => {
       },
     });
 
-    return { id, lat, lon };
+    return { id, lat, lon, aLat: 1, aLon: 1 };
   }
 
   onMounted(() => {
@@ -67,17 +69,23 @@ export const useMap = defineStore("map", () => {
     });
     map.on("load", async () => {
       rockets.push(addRocket("iss"));
-      // rockets.push(addRocket("iss2"));
-      // rockets.push(addRocket("iss3"));
+      rockets.push(addRocket("iss2"));
+      rockets.push(addRocket("iss3"));
 
       let previousTimeStamp = 0;
 
       function step(timestamp: number) {
         const per = timestamp - previousTimeStamp;
         rockets.forEach((rocket) => {
-          // rocket.lon = ((rocket.lon + per / 50 + 180) % 360) - 180;
-          rocket.lat = ((rocket.lat + per / 50 + 90) % 180) - 90;
-          console.log(rocket);
+          rocket.lon += per / 50;
+          rocket.lat += rocket.aLat * (per / 50);
+          if (rocket.lat > 90 || rocket.lat < -90) {
+            rocket.lat = rocket.aLat * 90 - (rocket.lat - rocket.aLat * 90);
+            rocket.lat = rocket.aLat * 90;
+            rocket.aLat *= -1;
+            rocket.lon += 180;
+          }
+          rocket.lon = ((rocket.lon + 180) % 360) - 180;
 
           const geojson = getLocation({ lon: rocket.lon, lat: rocket.lat });
           const iss = map.getSource(rocket.id) as GeoJSONSource;
@@ -93,3 +101,7 @@ export const useMap = defineStore("map", () => {
     });
   });
 });
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useMap, import.meta.hot));
+}
